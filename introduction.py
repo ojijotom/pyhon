3b811da6654a7f072a6d19b6a4224b2a11a3fe76
ojijotom <ojijotom4@gmail.com>
2025-02-04 14:29:18 +0300

    third commit

---
+++ introduction.py
@@ -0,0 +1,5 @@
+#print is a method used to display output
+print("Tom")
+print("Emobilis")
+print(100)
+print(20+10)
---
+++ variable.py
@@ -0,0 +1,19 @@
+#Avariable is a container used to store a value
+firstname = "Tom"
+lastname = "Ojijo"
+location = "westland"
+school = "emobilis"
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname,lastname)
+print("My full name is",firstname,lastname)
+x=39
+y=2
+print(x+y)
+product = x*y
+print(product)
+print("The poduct of",x,"and",y,"is",product)
+
